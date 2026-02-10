06d24a17ad6bb82c1fbc42e982a5263b9f44ca1f
Дмитрий Глоба <dima.globa2015@yandex.ru>
2026-02-10 19:50:52 +0800

    добавлены тесты product

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 
 from classes.category import Category
@@ -6,6 +8,7 @@ from classes.product import Product
 
 @pytest.fixture()
 def product_samsung():
+    """Фикстура Product"""
     return Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
 
 
@@ -39,3 +42,26 @@ def category_smartphones():
         description="Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни",
         products=products
     )
+
+@pytest.fixture()
+def product_samsung_new():
+    """Фикстура Product"""
+    return {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
+             "quantity": 5}
+
+@pytest.fixture()
+def product_samsung_price_up():
+    """Фикстура Product цена повышена"""
+    return {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 200000.0,
+             "quantity": 3}
+@pytest.fixture()
+def product_samsung_price_dp():
+    """Фикстура Product цена снижена"""
+    return {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 150000.0,
+             "quantity": 4}
+@pytest.fixture
+def mock_input():
+    """Фикстура для мока функции input()"""
+    def _mock_input(input_values):
+        return patch('builtins.input', side_effect=input_values)
+    return _mock_input
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,6 +1,52 @@
+from classes.product import Product
+
+
 def test_init_product(product_samsung):
     """Тест для инициирования в класс Product"""
     assert product_samsung.name == "Samsung Galaxy C23 Ultra"
     assert product_samsung.description == "256GB, Серый цвет, 200MP камера"
     assert product_samsung.price == 180000.0
     assert product_samsung.quantity == 5
+
+
+def test_new_product_class_method(product_samsung_new, product_samsung_price_up, product_samsung_price_dp):
+    """Тест для классового метода new_product"""
+
+    product1 = Product.new_product(product_samsung_new)
+    assert product1.name == "Samsung Galaxy S23 Ultra"
+    assert product1.price == 180000.0
+    assert product1.quantity == 5
+
+    product2 = Product.new_product(product_samsung_price_up)
+    assert product2.name == "Samsung Galaxy S23 Ultra"
+    assert product2.price == 200000.0
+    assert product2.quantity == 8
+
+    product3 = Product.new_product(product_samsung_price_dp)
+    assert product3.name == "Samsung Galaxy S23 Ultra"
+    assert product3.price == 200000.0
+    assert product3.quantity == 12
+
+
+def test_price_setter_increase(mock_input):
+    """Тест сеттера цены при повышении"""
+    product = Product("Samsung Galaxy S23", "256GB, Серый цвет, 100MP камера", 100000.0, 5)
+
+    product.price = 110000
+    assert product.price == 110000
+
+    product.price = -110000
+    assert product.price == 110000
+
+    with mock_input(["y"]):
+        product.price = 80000
+        assert product.price == 80000
+
+    with mock_input(["sdfsdf"]):
+        product.price = 70000
+        assert product.price == 80000
+
+
+
+
+
