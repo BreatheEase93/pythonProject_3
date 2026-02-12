59104aa6f53bd4c77c3bec81eb41fc2f5d932639
Дмитрий Глоба <dima.globa2015@yandex.ru>
2026-02-12 19:36:25 +0800

    Первая задача

---
+++ classes/lawngrass.py
@@ -0,0 +1,13 @@
+from classes.product import Product
+
+
+class LawnGrass(Product):
+    """Класс для Трава газонная"""
+
+    def __init__(self, name: str, description: str, price: float, quantity: int, country: str,
+                 germination_period: str, color: str) -> None:
+        """Инициирование объекта class LawnGrass"""
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
